909449226e30efddeefdc29607ca3dbb8a1fd3ae
Eder Souza <https://github.com/EderSouza93>
2024-03-07 13:01:47 -0300

    Grid completed

--- js/script.js
+++ js/script.js
@@ -3,12 +3,20 @@ const ctx = canvas.getContext("2d");
 
 const size = 30;
 
-const snake = [
-    { x: 200, y: 200 },
-    { x: 230, y: 200 }
-];
+const snake = [{ x: 270, y: 240 }];
+
+const food = {
+    x: 90,
+    y: 90,
+    color: "yellow"
+};
 
 let direction, loopId
+
+const drawFood = () => {
+    ctx.fillStyle = food.color
+    ctx.fillRect(food.x, food.y, size, size)
+}
 
 const drawSnake = () => {
     ctx.fillStyle = "#ddd";
@@ -35,22 +43,44 @@ const moveSnake = () => {
     }
 
     if (direction == "down") {
-        snake.push({ x: head.x, y: head.y + size})
+        snake.push({ x: head.x, y: head.y + size })
     }
 
     if (direction == "up") {
-        snake.push({ x: head.x, y: head.y - size})
+        snake.push({ x: head.x, y: head.y - size })
     }
 
     snake.shift()
 }
 
+
+const drawGrid = () => {
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "#191919";
+
+    for (let i = 30; i < canvas.width; i += 30) {
+        ctx.beginPath()
+        ctx.lineTo(i, 0);
+        ctx.lineTo(i, 600);
+        ctx.stroke();
+
+        ctx.beginPath()
+        ctx.lineTo(0, i);
+        ctx.lineTo(600, i);
+        ctx.stroke();
+    };
+
+};
+
+drawGrid();
+
 const gameLoop = () => {
     clearInterval(loopId)
-    ctx.clearRect(0, 0, 600, 600)
 
-    moveSnake()
-    drawSnake()
+    ctx.clearRect(0, 0, 600, 600)
+    drawGrid();
+    moveSnake();
+    drawSnake();
 
     loopId = setTimeout(() => {
         gameLoop()
@@ -60,19 +90,19 @@ const gameLoop = () => {
 gameLoop()
 
 document.addEventListener("keydown", ({ key }) => {
-   if (key == "ArrowRight" && direction != "left") {
+    if (key == "ArrowRight" && direction != "left") {
         direction = "right"
-   };
+    };
 
-   if (key == "ArrowLeft" && direction != "right") {
+    if (key == "ArrowLeft" && direction != "right") {
         direction = "left"
-   };
+    };
 
-   if (key == "ArrowDown" && direction != "up") {
+    if (key == "ArrowDown" && direction != "up") {
         direction = "down"
-   };
+    };
 
-   if (key == "ArrowUp" && direction != "down") {
+    if (key == "ArrowUp" && direction != "down") {
         direction = "up"
-   };
+    };
 })
